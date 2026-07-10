b594407cf86d2b9c898cd4c0de3e44718f28d789
YCP Dev <ycp@yachiyo.cn>
2026-07-10 14:18:15 +0800

    Release v1.1.2 - Testing auto updater

--- YCPLauncher/App.xaml.cs
+++ YCPLauncher/App.xaml.cs
@@ -7,7 +7,7 @@ namespace YCPLauncher;
 
 public partial class App : System.Windows.Application
 {
-    public static readonly string CurrentVersion = "1.1.1";
+    public static readonly string CurrentVersion = "1.1.2";
 
     private static System.Threading.Mutex? _mutex;
 
